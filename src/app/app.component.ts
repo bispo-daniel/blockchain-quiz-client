12ce84e33d1db3880df016cf48fac5671c311e6e
DBS <dbispo.work@gmail.com>
2025-05-09 00:26:34 -0300

    Add book animation and update text styles in slides

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import WebFont from 'webfontloader';
 WebFont.load({
   google: {
     families: [
-      'Modak:100,200,300,400,600,700,800,900'
+      'Modak:100,200,300,400,600,700,800,900',
+      'Roboto:100,200,300,400,600,700,800,900'
     ],
   },
 });
