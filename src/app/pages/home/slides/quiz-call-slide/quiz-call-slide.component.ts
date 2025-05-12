fb1a8015bd489b126f899eac03b8368784ef438d
DBS <dbispo.work@gmail.com>
2025-05-11 21:34:56 -0300

    Add QuizService and update quiz component to fetch questions from API

--- src/app/pages/home/slides/quiz-call-slide/quiz-call-slide.component.ts
+++ src/app/pages/home/slides/quiz-call-slide/quiz-call-slide.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-quiz-call-slide',
-  imports: [],
+  imports: [RouterLink],
   standalone: true,
   templateUrl: './quiz-call-slide.component.html',
   styleUrl: './quiz-call-slide.component.css',
--- src/app/pages/quiz/quiz.component.ts
+++ src/app/pages/quiz/quiz.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  CUSTOM_ELEMENTS_SCHEMA,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { QuizService, QuizQuestion } from '../../core/services/quiz.service';
 
 @Component({
   selector: 'app-quiz',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './quiz.component.html',
-  styleUrl: './quiz.component.css'
+  styleUrl: './quiz.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
+  questions: QuizQuestion[] = [];
+  picked: number[] = [];
 
+  @ViewChild('mainSwiper', { static: true })
+  mainSwiper!: ElementRef<HTMLElement>;
+
+  constructor(private qs: QuizService) {}
+
+  async ngOnInit() {
+    this.questions = await this.qs.getQuiz();
+    this.picked = Array(this.questions.length).fill(-1);
+  }
+
+  choose(qIdx: number, optIdx: number) {
+    this.picked[qIdx] = optIdx;
+
+    setTimeout(() => {
+      if (qIdx < this.questions.length - 1) {
+        (this.mainSwiper.nativeElement as any).swiper.slideNext();
+      }
+    }, 400);
+  }
+
+  get score() {
+    return this.questions.reduce(
+      (sum, q, i) => (this.picked[i] === q.correct ? sum + 1 : sum),
+      0
+    );
+  }
 }
